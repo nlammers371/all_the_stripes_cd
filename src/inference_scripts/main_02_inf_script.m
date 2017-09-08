90e1ad4c3136d2c6f980ffead5df4c487eef3280
nlammers371 <nlammers@berkeley.edu>
2017-09-08 10:11:07 -0700

    Script for s1

--- src/inference_scripts/main_02_inf_script.m
+++ src/inference_scripts/main_02_inf_script.m
@@ -1,10 +1,10 @@
 %Get environment variable from job script
-stripe_groups = {str2num(getenv('SLURM_ARRAY_TASK_ID'))};
-% stripe_groups = {1};
+% stripe_groups = {str2num(getenv('SLURM_ARRAY_TASK_ID'))};
+stripe_groups = {1};
 stripe_regions = [0];
 %only do stripe centers for now
 w = 8;
-state_vec = [3,2];
+state_vec = [3];
 %route to utilities folder
 addpath('../utilities');
 %------------------Define Inference Variables------------------------------%
